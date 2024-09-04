491b4e2a0576ccb8ba1baef77a959affb0be46a8
Ali <105427281+Alilock@users.noreply.github.com>
2024-09-04 18:33:32 +0400

    chore: Add JavaScript code for basic operations and data types

---
+++ JavaScript Intro/script.js
@@ -0,0 +1,108 @@
+// var number = 10
+
+// number *=5
+
+
+// var number = 15**2
+// var number= 10%3 
+// console.log(number);
+
+
+// var count = 1
+// console.log(count);
+// count =2 
+
+
+// var number = 4
+// if(number >= 4){
+//     console.log('number 4dur veya  boyukdur');
+// }
+// else if(number ==4){
+//     console.log('number 4 dur');
+// }
+// else if(number < 4){
+//     console.log('number 4 den kicikdir');
+// }
+// var number= 5
+// if(number > 6 || number >=5){
+//     console.log('true');
+// }
+// else{
+//     console.log('false');
+    
+// }
+// console.log();
+
+
+// var number = 1 
+
+// var number = 2
+// let number2 = 2
+//  number2 = 5
+// console.log(number2);
+// const number3 = 3
+// number3 *=5
+// console.log(number3);
+
+
+
+// alert("Message")
+// console.log(age+5);
+// const bool = confirm("Are you sure?")
+// console.log(bool);
+// let age = prompt("Yasinizi daxil edin")
+
+// document.write(age)
+
+// let string = "string"
+// let number = 5
+// let undefinedd = undefined
+// let nullable= null
+// let boolean= true
+// let bignint = 234n
+
+// console.log(typeof string);
+// console.log(typeof number);
+// console.log(typeof undefinedd);
+// console.log(typeof nullable);
+// console.log(typeof boolean);
+// console.log(typeof bignint);
+
+// let variable = -34
+// let variable = NaN
+
+
+// if(variable){
+//     console.log('truthy');
+// }else{
+//     console.log('falsy');
+// }
+
+
+
+
+// let bool = true===1
+
+// console.log('bool',bool);
+
+
+
+// if(bool !==1){
+//     console.log('duzdur');
+// }
+// else{
+//     console.log('sehvdir');
+// }
+
+
+
+// let promptinput= prompt("yasinizi yaz")
+// promptinput= Number(promptinput)
+// console.log(promptinput+5);
+// console.log(+"15"+ +"5");
+
+// console.log("Ali"+" "+"Nasrullayev");
+
+
+// let number = 5
+// console.log(++number);
