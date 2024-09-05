e533b4fddb2f0070c7d2ecf56a8fcbe94f415c6a
Ali <105427281+Alilock@users.noreply.github.com>
2024-09-05 21:04:47 +0400

    js

---
+++ JavaScript Functions/assets/script.js
@@ -0,0 +1,105 @@
+// let a = 5
+// let b = 7
+// let c = 14
+// const sumNumbers = Sum(a, b, c)
+// console.log('sum', sumNumbers);
+
+// function Sum(a, b, c = 0) {
+
+//     return a + b + c
+// }
+
+// const array = [23, 345, 1235, 56, 123, 98, 66, 79]
+
+// function findOddNumbers(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 != 0) {
+//             console.log(arr[i]);
+//         }
+//     }
+// }
+
+
+// findOddNumbers(array)
+// let maxNumber = Math.max(1, 2, 3, 4, 63, 213, 457, 123)
+
+// function CustomMax() {
+//     let maxNumber = arguments[0]
+
+
+//     for (let i = 1; i < arguments.length; i++) {
+//         if (arguments[i] > maxNumber) {
+//             maxNumber = arguments[i]
+//         }
+//     }
+//     return maxNumber
+// }
+
+// let max = CustomMax(4444, 12, 2, 1, 42, 1143, 54, 12, 43)
+// CustomMax(3, 2, 5, 2, 2123)
+
+// console.log(max);
+
+
+
+// function numbersOddsorEvens(array, cb) {
+//     let arr = []
+
+//     for (let i = 0; i < array.length; i++) {
+//         if (cb(array[i])) {
+//             arr.push(array[i])
+//         }
+//     }
+
+//     return arr
+// }
+
+// function isEven(number) {
+//     if (number % 2 == 0) {
+//         return true
+//     }
+// }
+// function isOdd(number) {
+//     if (number % 2 != 0) {
+//         return true
+//     }
+// }
+// const arr = [1, 2, 4, 5, 12, 6, 7, 8, 9]
+// numbersOddsorEvens(arr, (a) => {
+//     if (a % 2 == 0) {
+//         return true
+//     }
+// })
+
+
+
+// const Sum = (a, b) => {
+//     console.log(a + b);
+
+// }
+// Sum(10, 15)
+
+// function OddNumbers(array) {
+//     let arr = []
+
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] % 2 != 0) {
+//             arr.push(array[i])
+//         }
+//     }
+//     return arr
+// }
+
+
+// const evens = EvenNumbers([1, 2, 3, 4, 5, 6, 7, 8])
+// const odds = OddNumbers([1, 2, 3, 4, 5, 6, 7, 8])
+
+// console.log(evens);
+// console.log(odds);
+
+
+
+
+
+
+
---
+++ JavaScript Loops, Arrays, Switch Case/assets/script.js
@@ -0,0 +1,22 @@
+// let array = [1, 2, 3, "ali", true, null, "LTCLAB"]
+
+// for (let i = 0; i < array.length; i++) {
+//     console.log('arrayin elementi', array[i])
+// }
+
+// let day = prompt("Tarix elave edin:")
+// day = Number(day)
+// if (day <= 2 && day >= 1) {
+//     switch (day) {
+//         case 1:
+//             console.log('Monday');
+//             break;
+//         case 2:
+//             console.log('Tuesday');
+//             break;
+//         default:
+//             console.log('Duzgun tarix secin');
+//             break;
+//     }
+// }
+
