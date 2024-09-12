57a51c5cf1bdc3faf36caf37d6edc384db68d077
Ali <105427281+Alilock@users.noreply.github.com>
2024-09-12 20:45:56 +0400

    chore: Update JavaScript code for string methods and DOM manipulation

---
+++ 14. Js String Methods, Objects/assets/script.js
@@ -0,0 +1,56 @@
+let groupNo = "perrun 0011"
+let groupNo2 = "perrun 0012"
+// console.log(groupNo.slice(5));
+// console.log(groupNo.substring(3, 6));
+// console.log(groupNo.substr(2, 6));
+
+// console.log(groupNo.replace(/r/ig, "s"));
+
+// let capitalGroupNo = groupNo[0].toUpperCase() + groupNo.slice(1);
+// console.log(capitalGroupNo);
+
+// console.log(groupNo.concat("-", groupNo2));
+// let hello = "    Hello World    ";
+// console.log(hello.trim());
+// JavaScript Objects
+// let Person = {
+//     firstName: "Ali",
+//     lastName: "Khan",
+//     age: 25,
+//     getFullName: function () {
+//         return this.firstName + " " + this.lastName;
+//     }
+// }
+
+// // console.log(Person.getFullName());
+// Person.eyeColor = "blue";
+// Person.age = 30;
+
+// const keys = Object.keys(Person)
+// const values = Object.values(Person)
+// const entries = Object.entries(Person)
+// console.log(entries[1][1]);
+
+
+// Reference and Primitive Types 
+
+// let number = 5
+// let number2 = number
+
+// number2 = 10
+
+// console.log(number);
+// console.log(number2);
+
+
+// let student1 = {
+//     name: "Ali",
+//     age: 25
+// }
+// let student2 = Object.assign({}, student1)
+
+// student2.age = 30
+// console.log(student1);
+// console.log(student2);
+
+
---
+++ 15. Js DOM & BOM/assets/script.js
@@ -0,0 +1,48 @@
+// console.log(window.navigator);
+// console.log(window.location);
+// console.log(window.history.back);
+// console.log(window.navigator.geolocation.getCurrentPosition(
+//     (location) => {
+//         console.log('succes', location);
+
+//     },
+//     () => {
+//         console.log('error');
+
+//     }));
+
+
+// DOM
+
+// window.document
+// let headings = document.getElementsByTagName("h1")
+// let h1 = document.getElementsByClassName("first")
+// let h1 = document.getElementById("groupno")
+// const h1 = document.getElementsByName("groupno")
+
+// let headings = document.querySelectorAll("h1")
+
+
+// headings.forEach((h1) => {
+//     console.log(h1);
+
+// })
+
+
+
+// let headings = document.getElementsByTagName("h1")
+// // console.log(headings);
+// let headingsArr = Array.from(headings)
+// headingsArr.forEach((h1) => {
+//     console.log(h1);
+
+// })
+
+
+// setTimeout(() => {
+//     console.log("Hello");
+
+// }, 1000)
+
+// setInterval(() => {
+// }, 1000)
